5f711cafdbd5db21104385db21d1c5c5a70b8739
Julian Stang <julian.stang@tum.de>
2021-01-31 08:40:58 +0100

    Updated the base class

--- scraper/_base.py
+++ scraper/_base.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import abc
+
 __doc__ = """The base class.
 """
 
 
-class Scraper:
+class Scraper(abc.ABC):
     """The base scraper class.
     """
 
     def __init__(self):
         pass
+
+    @abc.abstractmethod
+    def load(self):
+        """Abstract method for loading url content.
+        """
+
+    @abc.abstractmethod
+    def parse(self):
+        """Abstract method for parsing url content.
+        """
