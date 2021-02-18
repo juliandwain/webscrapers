804f41f9a0a77a2f227b027bc0fcfd4ca29edf0c
Julian Stang <julian.stang@tum.de>
2021-02-18 07:24:35 +0100

    Added setup file

---
+++ setupy.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="webscrapers",
+    version="0.0.1",
+    author="Julian Dwain Stang",
+    author_email="julian.stang@web.de",
+    description=("A simple webscraping framework."),
+    license="MIT",
+    url="https://github.com/juliandwain/webscrapers",
+    packages=find_packages(),
+    long_description=read('README'),
+)
